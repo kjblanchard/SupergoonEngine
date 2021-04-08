bacdd8b5ad01e3ce9f53f9d0fef5b8ab900fca50
Kevin Blanchard <blanchardkevinj@gmail.com>
2021-04-08 17:55:44 -0400

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SgEngine
+{
+    public class Class1
+    {
+        public static void PrintGarbage()
+        {
+            Debug.WriteLine("no u");
+
+        }
+    }
+}
