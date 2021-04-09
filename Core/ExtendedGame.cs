f86d5fcbdb63eb676f0ed085462b9fa6c5fc3061
Kevin Blanchard <blanchardkevinj@gmail.com>
2021-04-08 22:36:48 -0400

    Add in Sound

---
+++ Core/ExtendedGame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SgEngine.Core
+{
+   public class ExtendedGame : Game
+    {
+    }
+}
---
+++ Interfaces/IUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SgEngine.Interfaces
+{
+    interface IUpdate
+    {
+        virtual void Startup(){}
+
+        virtual void Update(GameTime gameTime){}
+        virtual void HandleInput (){}
+
+        virtual void Draw(SpriteBatch spriteBatch){}
+    }
+}
