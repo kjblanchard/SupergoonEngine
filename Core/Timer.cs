73d5cfe23c5078a2f308281111ad7ce0bf913068
Kevin Blanchard <blanchardkevinj@gmail.com>
2021-04-17 22:52:41 -0400

    added in tween for floats

--- Core/Timer.cs
+++ Core/Timer.cs
@@ -14,21 +14,32 @@ namespace SgEngine.Core
     {
 
 
-        private int msLeftToWait;
-        private Action _theActionToPerform;
-        private bool _timerCompleted;
+        protected int _totalMsWaited = 0;
+        protected int _msStartingTime;
+        protected readonly Action _theActionToPerform;
+        protected readonly Action<float> _theOtherActiontoPerform;
+
+        private bool _timerCompleted = false;
+        //TODO move this implementation as abstract, and then make two timers
         public Timer(int msTowait, Action funcToUse)
         {
-            msLeftToWait = msTowait;
+            _msStartingTime = msTowait;
             _theActionToPerform = funcToUse;
+        }
+        public Timer(int msTowait, Action<float> funcToUse)
+        {
+            _msStartingTime = msTowait;
+            _theOtherActiontoPerform = funcToUse;
         }
 
 
-        public void Update(GameTime gameTime)
+
+
+        public virtual void Update(GameTime gameTime)
         {
             if (!_timerCompleted)
-                msLeftToWait -= gameTime.ElapsedGameTime.Milliseconds;
-            if (msLeftToWait <= 0)
+                _totalMsWaited += gameTime.ElapsedGameTime.Milliseconds;
+            if (_totalMsWaited >= _msStartingTime)
             {
                 _theActionToPerform();
             }
---
+++ Core/TweenTimer.cs
@@ -0,0 +1,48 @@
+﻿////////////////////////////////////////////////////////////
+//
+// Super Goon Games - 2D Game Engine
+// Copyright (C) 2020-2021 - Kevin Blanchard
+//
+////////////////////////////////////////////////////////////
+
+using System;
+using Microsoft.Xna.Framework;
+
+namespace SgEngine.Core
+{
+    public class TweenTimer : Timer
+    {
+        private readonly float _startingValue;
+        private readonly float _endingValue;
+        private readonly float _diff;
+       public TweenTimer(float startingValue, float endingValue, int msTowait, Action<float> funcToUse) : base(msTowait, funcToUse)
+        {
+            _startingValue = startingValue;
+            _endingValue = endingValue;
+            _diff = _endingValue - _startingValue;
+            
+        }
+
+
+
+        public override void Update(GameTime gameTime)
+        {
+
+            if(_totalMsWaited > _msStartingTime)
+                return;
+            _totalMsWaited += gameTime.ElapsedGameTime.Milliseconds;
+            _theOtherActiontoPerform(calculateValue());
+        }
+
+        private float calculateValue()
+        {
+
+            if (_totalMsWaited == 0)
+                return 0;
+            var percentageOfProgress = _totalMsWaited  *1.0 / _msStartingTime;
+            var progressAddition = _diff * percentageOfProgress;
+            return (float)(_startingValue + progressAddition);
+        }
+
+    }
+}
