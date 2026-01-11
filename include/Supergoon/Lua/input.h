91e89ddf43a7f5822a385ed4bd37b20064859a7d
Kevin Blanchard <kevin@supergoon.com>
2026-01-11 15:09:43 -0500

    remove lua files

--- include/Supergoon/Lua/input.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifdef __cplusplus
-extern "C" {
-#endif
-void RegisterLuaInputFunctions(void);
-
-#ifdef __cplusplus
-}
-#endif
--- include/Supergoon/Lua/scene.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifdef __cplusplus
-extern "C" {
-#endif
-void RegisterLuaSceneFuncs(void);
-#ifdef __cplusplus
-}
-#endif
