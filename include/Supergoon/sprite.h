21a7025596c7b43fc62fb585974d5d58a389c8a7
Kevin Blanchard <kevin@supergoon.com>
2026-02-01 22:18:41 -0500

    ui animation

--- include/Supergoon/sprite.h
+++ include/Supergoon/sprite.h
@@ -5,11 +5,13 @@ extern "C" {
 #endif
 typedef struct Texture Texture;
 typedef struct Shader Shader;
+typedef struct Color Color;
 struct sgGameObject;
 typedef enum SpriteFlags {
 	SpriteFlagVisible = 1 << 0,
 	SpriteFlagDestroyed = 1 << 1,
 } SpriteFlags;
+
 typedef struct Sprite {
 	float* parentX;
 	float* parentY;
@@ -22,7 +24,11 @@ typedef struct Sprite {
 } Sprite;
 
 Sprite* NewSprite(void);
+// This doesn't get drawn during the libraries, useful for UI or something lke that.
+Sprite* NewSpriteManual(void);
 void DestroySprite(Sprite* sprite);
+void DestroySpriteManual(Sprite* sprite);
+void DrawSpriteManual(Sprite* sprite, RectangleF* dstRect, Color* color);
 void DrawSpriteSystem();
 void ShutdownSpriteSystem(void);
 #ifdef __cplusplus
